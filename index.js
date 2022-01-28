bea76e80a8a8653fa20aff53f9b908b0756d9ec7
BasmaHeshamMahmoud <basmahesham778@gmail.com>
2022-01-28 20:20:56 +0200

    Second Commit

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ app.get("/", (req, res) => {
   res.send("Hi from NodeJS Project");
 });
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000, () => {
+  console.log("listening ... ");
+});
